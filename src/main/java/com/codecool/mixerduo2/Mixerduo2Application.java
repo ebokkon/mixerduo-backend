40e12ece5919fb98f9c51ba19159924cddbfb6af
Agnes Kadlot <agnes.kadlot@gmail.com>
2020-02-04 10:13:55 +0100

    Initial commit

---
+++ src/main/java/com/codecool/mixerduo2/Mixerduo2Application.java
@@ -0,0 +1,13 @@
+package com.codecool.mixerduo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mixerduo2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Mixerduo2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/mixerduo2/Mixerduo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.mixerduo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mixerduo2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
