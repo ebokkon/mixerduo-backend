1365fd71ef45f560a2c958135ce3c7ec74b10cc6
Agnes Kadlot <agnes.kadlot@gmail.com>
2020-03-04 13:52:27 +0100

    Customize responses when authenticating

--- src/main/java/com/codecool/mixerduo2/controller/AuthController.java
+++ src/main/java/com/codecool/mixerduo2/controller/AuthController.java
@@ -1,16 +1,13 @@
 package com.codecool.mixerduo2.controller;
 
 import com.codecool.mixerduo2.model.Client;
-import com.codecool.mixerduo2.restexceptionhandling.UserAlreadyExistAuthenticationException;
 import com.codecool.mixerduo2.model.UserCredentials;
 import com.codecool.mixerduo2.repository.ClientRepository;
 import com.codecool.mixerduo2.security.JwtTokenServices;
 import com.codecool.mixerduo2.security.PasswordEncoderService;
 import com.codecool.mixerduo2.service.DataValidateService;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -47,9 +44,10 @@ public class AuthController {
     }
 
     @PostMapping("/sign_up")
-    public ResponseEntity registerUser(@RequestBody UserCredentials userCredentials) throws UserAlreadyExistAuthenticationException {
+    public ResponseEntity registerUser(@RequestBody UserCredentials userCredentials) {
         String username = userCredentials.getUsername();
         String password = userCredentials.getPassword();
+        Map<Object, Object> model = new HashMap<>();
 
         //check valid password data
         List<String> errorList = new ArrayList<>();
@@ -57,23 +55,27 @@ public class AuthController {
         if(!dataValidateService.isValid(password, errorList)){
             String error = String.join(",", errorList);
             String errorMessage = "The password must contain at least " + error + "!";
-//            ResponseEntity.badRequest().body(errorList);
-            return new ResponseEntity(errorMessage, HttpStatus.BAD_REQUEST);
+            model.put("correct", false);
+            model.put("msg", errorMessage);
+            return ResponseEntity.ok(model);
         };
         //check if username exists already
         if (clientRepository.findByUsername(username).isPresent()){
-            throw new UserAlreadyExistAuthenticationException("Username already exists!");
+            model.put("correct", false);
+            model.put("msg", "Username already exists!");
+            return ResponseEntity.ok(model);
         } else {
             //save to db
             Client client = Client.builder().username(username).password(pwService.encodePassword(password)).build();
             clientRepository.save(client);
         }
-        return new ResponseEntity(HttpStatus.OK);
+        model.put("correct", true);
+        return ResponseEntity.ok(model);
     }
-
 
     @PostMapping("/sign_in")
     public ResponseEntity signin(@RequestBody UserCredentials userCredentials){
+        Map<Object, Object> model = new HashMap<>();
         try{
             String username = userCredentials.getUsername();
             String password = userCredentials.getPassword();
@@ -81,14 +83,15 @@ public class AuthController {
             List<String> roles = authentication.getAuthorities().stream()
                     .map(GrantedAuthority::getAuthority).collect(Collectors.toList());
             String token = jwtTokenServices.createToken(username, roles);
-            Map<Object, Object> model = new HashMap<>();
+            model.put("correct", true);
             model.put("username", username);
             model.put("roles", roles);
             model.put("token", token);
-            System.out.println(ResponseEntity.ok(model));
             return ResponseEntity.ok(model);
         } catch (AuthenticationException e){
-            throw new BadCredentialsException("Wrong username/password");
+            model.put("correct", false);
+            model.put("msg", "Wrong username/password");
+            return ResponseEntity.ok(model);
         }
     }
 }
