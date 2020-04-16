598e1102a9b8ccc8acbb623a10db6a053ab94352
Agnes Kadlot <agnes.kadlot@gmail.com>
2020-04-16 15:10:30 +0200

    ensuring email is unique at sign up

--- apigateway/src/main/java/com/codecool/apigateway/repository/ClientRepository.java
+++ apigateway/src/main/java/com/codecool/apigateway/repository/ClientRepository.java
@@ -12,4 +12,5 @@ public interface ClientRepository extends JpaRepository<Client, Long> {
 
     Optional<Client> findByUsername(String username);
 
+    Optional<Client> findByEmail(String email);
 }
